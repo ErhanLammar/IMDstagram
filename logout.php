1b30fba57232a590191085449820b94f93a52c95
Erhan Lammar <ErhanLammar@users.noreply.github.com>
2016-05-14 15:36:28 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -13,11 +13,11 @@ include_once ("classes/Db.class.php");
 session_start();
 if(session_destroy())
 {
-    
+
     $destroyable = 'loggedIn';
     destroy();
     echo $destroyable;
-    
+
     //logged in return to index page
     header("Location: login.php");
 }
